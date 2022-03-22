3fb9bb0d5ea2a46f54883bf959f572e270f85706
Nikita Dyachenko <nikita.dyachenko93@bk.ru>
2022-03-22 17:57:43 +0300

    Урок 1 - добавили object class и потестили циклы

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,7 +14,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonTest()
+        dataTest()
+        objectTest()
+        getList()
+    }
 
+
+    private fun objectTest() {
+        val textView03: TextView = findViewById(R.id.testTextViewObject)
+        val objectText: Int
+        objectText = ObjectTest.getName()
+        textView03.text = objectText.toString()
+    }
+
+
+    private fun dataTest() {
+        val weather = Weather("Москва", 10)
+        val weather02 = weather.copy()
+        val textViewWeather: TextView = findViewById(R.id.testTextView)
+        textViewWeather.setText("${weather02.townName}" + " + " + "${weather02.temperature}")
+    }
+
+
+    private fun buttonTest() {
         val textView: TextView = findViewById(R.id.testTextViewButton)
 
         val button: Button = findViewById(R.id.testButton);
@@ -29,16 +52,24 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
-        val weather = Weather ("Москва", 10)
-        val textViewWeather: TextView = findViewById(R.id.testTextView)
-        textViewWeather.setText("${weather.townName}" + " + " + "${weather.temperature}")
+    }
 
+    fun getList() {
+        val list = listOf("1", "2", "3", "4", "5", "6", "7")
+        /* for (a in list) {
+             Log.d("myLogs", "$a")
+             val textView04: TextView = findViewById(R.id.testTextViewCircle)
+             textView04.append(a + " ")
 
-
+         */
+        val textView04: TextView = findViewById(R.id.testTextViewCircle)
+        list.forEach { textView04.append(it + " ") }
 
 
     }
+
 }
+
 
 
 
---
+++ app/src/main/java/com/example/myapplication/ObjectTest.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+
+object ObjectTest {
+
+
+    fun getName(): Int {
+        var name01: Int = 0;
+        return name01
+    }
+}
