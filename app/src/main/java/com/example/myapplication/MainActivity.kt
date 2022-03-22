73c39f0ae6dbdf99ae8ce2dac0e0514f435bdcc6
Nikita Dyachenko <nikita.dyachenko93@bk.ru>
2022-03-22 15:15:43 +0300

    Урок 1 - добавили кнопку и повесли обработчик

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var position: Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.testTextView)
+
+        val button: Button = findViewById(R.id.testButton);
+        button.setOnClickListener {
+
+            if (position) {
+                textView.text = "Добро пожаловать в Котлин"
+                position = false
+
+            } else {
+                textView.text = "Отвали, я уже все сказал"
+            }
+
+        }
     }
 }
+
